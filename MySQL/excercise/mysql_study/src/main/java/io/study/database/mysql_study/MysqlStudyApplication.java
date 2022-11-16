def24298deff635495d4e61d35a7afc8a059dc51
soon.good <soon.good@outlook.com>
2022-11-16 16:31:05 +0900

    샘플데이터 연습을 위한 프로젝트
    = 샘플데이터 insert 등 여러가지를 위한 용도의 프로젝트

---
+++ MySQL/excercise/mysql_study/src/main/java/io/study/database/mysql_study/MysqlStudyApplication.java
@@ -0,0 +1,13 @@
+package io.study.database.mysql_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlStudyApplication.class, args);
+	}
+
+}
